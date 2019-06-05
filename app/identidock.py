c79ce5e2e7b01d5be81ee41923301ef366662143
Prod <eg@anohin.info>
2019-06-05 11:03:32 +0300

    del test str

--- app/identidock.py
+++ app/identidock.py
@@ -26,7 +26,7 @@ def main_page():
               <img src="/monster/{}"/>
               '''.format(name, name_hash)
     footer = '</body></html>'
-    return header + body + footer + "TEST systemd"
+    return header + body + footer
 
 
 @app.route('/monster/<name>')
